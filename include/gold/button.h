611785f5a162032ce808101ccae0b0c64f262960
Josie Thompson <dot.slash.josie@gmail.com>
2022-01-18 14:45:36 -0800

    added texture rendering to button

--- include/gold/button.h
+++ include/gold/button.h
@@ -12,8 +12,13 @@ namespace au {
 class button : public iwidget {
 public:
     button() = delete;
-    button(SDL_Rect const & bounds, SDL_Color const & border_color,
-           SDL_Color const & fill_color, std::uint32_t border_thickness);
+    button(SDL_Rect const & bounds,
+
+           SDL_Color const & border_color, SDL_Color const & fill_color,
+           std::uint32_t border_thickness,
+
+           SDL_Texture * content,
+           std::uint32_t horizontal_padding, std::uint32_t vertical_padding);
 
     // interface methods
     inline SDL_Rect bounds() const { return _bounds; }
@@ -24,7 +29,20 @@ private:
     SDL_Color _fill_color;
     int _border_thickness;
 
+    SDL_Texture * _content;
+    int _horizontal_padding;
+    int _vertical_padding;
+
     // get the bounding rect of the button inside the border
     SDL_Rect _inner_bounds() const;
+
+    // get the maximum bounding rect for the inner content
+    SDL_Rect _max_content_bounds() const;
+
+    // get the bounding rect to draw the texture to
+    SDL_Rect _content_bounds() const;
+
+    // get the bounding rect to draw the texture from
+    SDL_Rect _clipped_texture_bounds() const;
 };
 }
--- src/button.cc
+++ src/button.cc
@@ -10,11 +10,22 @@ using uint = std::uint32_t;
 
 namespace au {
 
-button::button(SDL_Rect const & bounds, SDL_Color const & border_color,
-               SDL_Color const & fill_color, uint border_thickness)
+button::button(SDL_Rect const & bounds,
 
-    : _bounds(bounds), _border_color(border_color), _fill_color(fill_color),
-      _border_thickness(static_cast<int>(border_thickness))
+               SDL_Color const & border_color, SDL_Color const & fill_color,
+               uint border_thickness,
+               
+               SDL_Texture * content,
+               uint horizontal_padding, uint vertical_padding)
+
+    : _bounds(bounds),
+    
+      _border_color(border_color), _fill_color(fill_color),
+      _border_thickness(static_cast<int>(border_thickness)),
+
+      _content(content),
+      _horizontal_padding(static_cast<int>(horizontal_padding)),
+      _vertical_padding(static_cast<int>(vertical_padding))
 {
 }
 
@@ -30,6 +41,11 @@ void button::render(SDL_Renderer * renderer)
     SDL_SetRenderDrawColor(renderer, _fill_color.r, _fill_color.g,
                                      _fill_color.b, _fill_color.a);
     SDL_RenderFillRect(renderer, &inner_bounds);
+
+    // render the button content
+    SDL_Rect const src = _clipped_texture_bounds();
+    SDL_Rect const dst = _content_bounds();
+    SDL_RenderCopy(renderer, _content, &src, &dst);
 }
 
 SDL_Rect button::_inner_bounds() const
@@ -38,5 +54,56 @@ SDL_Rect button::_inner_bounds() const
         _bounds.x + _border_thickness, _bounds.y + _border_thickness,
         _bounds.w - 2*_border_thickness, _bounds.h - 2*_border_thickness
     };
+}
+
+SDL_Rect button::_max_content_bounds() const
+{
+    SDL_Rect const inner_bounds = _inner_bounds();
+    return {
+        inner_bounds.x + _horizontal_padding,
+        inner_bounds.y + _vertical_padding,
+        inner_bounds.w - 2*_horizontal_padding,
+        inner_bounds.h - 2*_vertical_padding
+    };
+}
+
+SDL_Rect button::_content_bounds() const
+{
+    SDL_Rect const max_bounds = _max_content_bounds();
+
+    // normalize the texture height to the max bounds height
+    int texture_width = 0;
+    int texture_height = 0;
+    SDL_QueryTexture(_content, nullptr, nullptr,
+                               &texture_width, &texture_height);
+    double const resize_ratio =
+        max_bounds.h / static_cast<double>(texture_height);
+    int const content_width = static_cast<int>(texture_width * resize_ratio);
+
+    return {
+        max_bounds.x, max_bounds.y,
+        std::min(max_bounds.w, content_width), max_bounds.h
+    };
+}
+
+SDL_Rect button::_clipped_texture_bounds() const
+{
+    SDL_Rect const max_bounds = _max_content_bounds();
+
+    // normalize the texture height to the max bounds height
+    int texture_width = 0;
+    int texture_height = 0;
+    SDL_QueryTexture(_content, nullptr, nullptr,
+                               &texture_width, &texture_height);
+    double const resize_ratio =
+        max_bounds.h / static_cast<double>(texture_height);
+
+    // clip the texture width if it's too long
+    double const content_width = texture_width * resize_ratio;
+    if (content_width > max_bounds.w) {
+        texture_width = static_cast<int>(max_bounds.w/resize_ratio);
+    }
+
+    return { 0, 0, texture_width, texture_height };
 }
 }
