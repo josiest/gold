0e936b659aff98d851d89b388d7897bffcf0f814
Josie Thompson <dot.slash.josie@gmail.com>
2022-01-18 15:22:26 -0800

    simple example now uses ion

--- examples/simple/simple.cc
+++ examples/simple/simple.cc
@@ -2,6 +2,7 @@
 #include <SDL.h>
 #include <SDL_ttf.h>
 #include <gold/gold.h>
+#include <ion/ion.hpp>
 
 // constants
 #include <cstdlib> // EXIT_SUCCESS, EXIT_FAILURE
@@ -15,77 +16,40 @@ namespace fs = std::filesystem;
 
 int main()
 {
-    // initialize sdl
-    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-        std::cout << "Unable to initialize SDL! " << SDL_GetError() << std::endl;
+    // create the sdl event-handler: quit when sdl's quit event is triggered
+    ion::event_system events;
+    events.subscribe(SDL_QUIT, &ion::input::quit_on_event);
+
+    // initialize sdl and ttf
+    ion::sdl_context sdl;
+    sdl.init_ttf();
+    if (not sdl) {
+        std::cout << sdl.get_error() << std::endl;
         return EXIT_FAILURE;
     }
 
-    // create a simple sdl window
-    SDL_Window * window = SDL_CreateWindow(
-            "Simple Example",
-            SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
-            640, 480, 0
-            );
+    // create a basic window, specifying the title and dimensions
+    auto window = ion::hardware_renderer::basic_window("Simple Example", 800, 600);
     if (not window) {
-        std::cout << "Couldn't create a window! " << SDL_GetError() << std::endl;
-        SDL_Quit();
-        return EXIT_FAILURE;
-    }
-
-    // create a basic hardware renderer
-    SDL_Renderer * renderer = SDL_CreateRenderer(
-            window, -1, SDL_RENDERER_ACCELERATED
-            );
-    if (not renderer) {
-        std::cout << "Couldn't create a renderer! " << SDL_GetError() << std::endl;
-        SDL_DestroyWindow(window);
-        SDL_Quit();
-        return EXIT_FAILURE;
-    }
-
-    // initialize ttf
-    if (TTF_Init() < 0) {
-        std::cout << "Couldn't initialize ttf! " << TTF_GetError() << std::endl;
-        SDL_DestroyRenderer(renderer);
-        SDL_DestroyWindow(window);
-        SDL_Quit();
+        std::cout << window.get_error() << std::endl;
         return EXIT_FAILURE;
     }
 
     // Load a font
     fs::path const font_path = fs::absolute("../assets/fonts/DejaVuSans.ttf");
-    TTF_Font * dejavu_sans = TTF_OpenFont(font_path.c_str(), 100);
+    ion::font dejavu_sans = ion::font::from_file(font_path, 100);
     if (not dejavu_sans) {
-        std::cout << "Couldn't open font! " << TTF_GetError() << std::endl;
-        SDL_DestroyWindow(window);
-        SDL_DestroyRenderer(renderer);
-        TTF_Quit(); SDL_Quit();
+        std::cout << dejavu_sans.get_error() << std::endl;
         return EXIT_FAILURE;
     }
 
     // Create the text for the button
     SDL_Color const black{0, 0, 0, 0xff};
-    SDL_Surface * text_surface = 
-        TTF_RenderText_Solid(dejavu_sans, "Click Me!", black);
-    if (not text_surface) {
-        std::cout << "Couldn't render text! " << TTF_GetError() << std::endl;
-        TTF_CloseFont(dejavu_sans); TTF_Quit();
-        SDL_DestroyWindow(window); SDL_DestroyRenderer(renderer);
-        SDL_Quit();
-        return EXIT_FAILURE;
-    }
-
-    // convert the text surface to a texture
-    SDL_Texture * rendered_text =
-        SDL_CreateTextureFromSurface(renderer, text_surface);
-    SDL_FreeSurface(text_surface);
+    ion::texture rendered_text =
+        dejavu_sans.render_text(window, "Click Me!", black);
     if (not rendered_text) {
-        std::cout << "Couldn't convert text surface to txture! "
-                  << SDL_GetError() << std::endl;
-        TTF_CloseFont(dejavu_sans); TTF_Quit();
-        SDL_DestroyWindow(window); SDL_DestroyRenderer(renderer);
-        SDL_Quit();
+        std::cout << rendered_text.get_error() << std::endl;
+        return EXIT_FAILURE;
     }
 
     // create a button
@@ -105,19 +69,12 @@ int main()
         }
 
         // clear the screen white
-        SDL_SetRenderDrawColor(renderer, 0xff, 0xff, 0xff, 0xff);
-        SDL_RenderClear(renderer);
+        SDL_SetRenderDrawColor(window, 0xff, 0xff, 0xff, 0xff);
+        SDL_RenderClear(window);
 
         // draw the button and update the screen
-        simple_button.render(renderer);
-        SDL_RenderPresent(renderer);
+        simple_button.render(window);
+        SDL_RenderPresent(window);
     }
-
-    SDL_DestroyTexture(rendered_text);
-    TTF_CloseFont(dejavu_sans);
-    TTF_Quit();
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
     return EXIT_SUCCESS;
 }
