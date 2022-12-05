3495009bc5e6875e2bb37ba923b77a69be338e7d
Josie Thompson <dot.slash.josie@gmail.com>
2022-12-04 21:52:48 -0800

    simple centered laveneder square

--- examples/ui-example.cpp
+++ examples/ui-example.cpp
@@ -6,6 +6,7 @@
 // data types and structure
 #include <string>
 #include <vector>
+#include <unordered_map>
 
 // serialization and i/o
 #include <yaml-cpp/yaml.h>
@@ -28,13 +29,59 @@ void print_error(std::string const & message) {
     std::cout << message << "\n\n";
 }
 
+namespace global {
+bool show_demo = false;
+}
+
 void render(SDL_Window *)
 {
+    if (global::show_demo) {
+        ImGui::ShowDemoWindow(&global::show_demo);
+    }
     if (not ImGui::NewWindow()) {
         ImGui::End();
         return;
     }
+
+    // draw example widget centered
+    ImVec4 const lavender{0.429f, 0.160f, 0.480f, 0.540f};
+    ImGui::PushStyleColor(ImGuiCol_ChildBg, lavender);
+
+    ImVec2 const size{ 100.f, 100.f };
+    float const avail_width = ImGui::GetContentRegionAvail().x;
+    float const x_offset = (avail_width - size.x)/2.f;
+    ImGui::SetCursorPosX(ImGui::GetCursorPosX() +  x_offset);
+
+    ImGui::BeginChild("example widget", size);
+    ImGui::EndChild();
+    ImGui::PopStyleColor();
+
     ImGui::End();
+}
+
+template<std::uint16_t mask>
+inline constexpr bool mask_contains(std::uint16_t mod)
+{
+    std::uint16_t constexpr all = KMOD_SHIFT | KMOD_ALT | KMOD_CTRL;
+    mod &= all;
+    return (mod & mask) == mod;
+}
+
+template<std::uint16_t mask>
+inline constexpr bool has_mask(std::uint16_t mod)
+{
+    return (mod & mask) != KMOD_NONE;
+}
+
+void toggle_demo(SDL_Keysym const & sym)
+{
+    auto constexpr demo_mask = KMOD_CTRL;
+    bool const can_toggle = mask_contains<demo_mask>(sym.mod) and
+                            has_mask<KMOD_CTRL>(sym.mod);
+
+    if (sym.sym == SDLK_d and can_toggle) {
+        global::show_demo = not global::show_demo;
+    }
 }
 
 int main()
@@ -50,5 +97,7 @@ int main()
     }
 
     system->on_render().connect<&render>();
+    system->on_keydown().connect<&toggle_demo>();
     system->start();
+    return EXIT_SUCCESS;
 }
