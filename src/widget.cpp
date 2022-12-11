9d614dc566d6c48e523ceb6cb881bc98e88485ac
Josie Thompson <dot.slash.josie@gmail.com>
2022-12-11 12:49:32 -0800

    fixed bug where default components aren't written

--- src/widget.cpp
+++ src/widget.cpp
@@ -15,25 +15,16 @@ gold::write(YAML::Emitter & out, entt::registry const & widgets,
 {
     out << YAML::Block << YAML::BeginMap;
     if (auto const * layout = widgets.try_get<gold::layout>(widget)) {
-        gold::layout const default_layout;
-        if (default_layout != *layout) {
-            out << YAML::Key << "align"
-                << YAML::Value << YAML::Flow << YAML::Node{ *layout };
-        }
+        out << YAML::Key << "align"
+            << YAML::Value << YAML::Flow << YAML::Node{ *layout };
     }
     if (auto const * size = widgets.try_get<gold::size>(widget)) {
-        gold::size const default_size;
-        if (gold::sq_dist(*size, default_size) >= .01f) {
-            out << YAML::Key << "size"
-                << YAML::Value << YAML::Flow << YAML::Node{ *size };
-        }
+        out << YAML::Key << "size"
+            << YAML::Value << YAML::Flow << YAML::Node{ *size };
     }
     if (auto const * color = widgets.try_get<gold::background_color>(widget)) {
-        gold::background_color const default_color;
-        if (gold::sq_dist(*color, default_color) > .01f) {
-            out << YAML::Key << "bg-color"
-                << YAML::Value << YAML::Flow << YAML::Node{ *color };
-        }
+        out << YAML::Key << "bg-color"
+            << YAML::Value << YAML::Flow << YAML::Node{ *color };
     }
     return out << YAML::EndMap;
 }
